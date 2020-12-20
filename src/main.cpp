ab4a65b1da7993fdf4d617e239f5169ab23dd561
balazs <blase.13@gmail.com>
2020-12-20 20:19:36 +0100

    power level update, IOCap update

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@ void setup(void)
     Serial.begin(115200);
     Serial.println("Starting NimBLE Server");
     NimBLEDevice::init("NimBLE");
+    NimBLEDevice::setPower(ESP_PWR_LVL_P9);
 
     NimBLEDevice::setSecurityAuth(true, true, true);
     NimBLEDevice::setSecurityPasskey(123356);
-    NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY);
+    NimBLEDevice::setSecurityIOCap(BLE_HS_IO_KEYBOARD_ONLY);
     NimBLEServer *pServer = NimBLEDevice::createServer();
     NimBLEService *pService = pServer->createService("ABCD");
     NimBLECharacteristic *pCharacteristic = pService->createCharacteristic("1234", NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::READ_ENC);
